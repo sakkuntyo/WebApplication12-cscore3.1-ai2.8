17848fcc0aa81b3be358d5eb0d196ecc256455c3
sakkuntyo <20591351+sakkuntyo@users.noreply.github.com>
2020-11-16 23:05:19 +0900

    プロジェクト ファイルを追加します。

---
+++ WebApplication12-cscore3.1-ai2.8/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication12_cscore3._1_ai2._8.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
